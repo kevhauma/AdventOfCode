c1619d397a0bf72299ae981711607dacca29d746
kevin hauman <kevin.hauman@zinderlabs.com>
2022-12-02 10:57:06 +0100

    one-lined day2/p2

--- day2/puzzle.js
+++ day2/puzzle.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
 
 const YOU = { A: 1, B: 2, C: 3 }; // rock, paper, sciccor
-const ME = { X: 1, Y: 2, Z: 3 }; //
+const ME = { X: 1, Y: 2, Z: 3 };
 const RESULT = { LOSE: "X", TIE: "Y", WIN: "Z" };
-const points = {X: 1, Y: 2, Z: 3, W: 6, L: 0, T: 3 };
+const points = {X: ME.X, Y: ME.Y, Z: ME.Z, W: 6, L: 0, T: 3, TIE: {A:ME.X,B:ME.Y,C:ME.Z}, WIN:{A:ME.Y,B:ME.Z,C:ME.X}, LOSE:{A:ME.Z,B:ME.X,C:ME.Y}};
 
 const prepareData = () => fs
     .readFileSync("./day2/input.txt", { encoding: "utf8" })
@@ -28,33 +28,13 @@ const p1 = () =>
 /*
 Part two
 */
-const p2 = () => {
- const rounds = prepareData();
-  let score = 0;
-  rounds.forEach((r) => {
-    //TIE
-    if (r.me === RESULT.TIE) {
-      score += points.T;
-      if (r.you == YOU.ROCK) score += points[ME.ROCK];
-      if (r.you == YOU.SCICCOR) score += points[ME.SCICCOR];
-      if (r.you == YOU.PAPER) score += points[ME.PAPER];
-    }
-    //LOSE
-    if (r.me === RESULT.LOSE) {
-      score += points.L;
-      if (r.you == YOU.ROCK) score += points[ME.SCICCOR];
-      if (r.you == YOU.SCICCOR) score += points[ME.PAPER];
-      if (r.you == YOU.PAPER) score += points[ME.ROCK];
-    }
-    //WIN
-     if (r.me === RESULT.WIN) {
-       score += points.W;
-       if (r.you == YOU.ROCK) score += points[ME.PAPER];
-       if (r.you == YOU.SCICCOR) score += points[ME.ROCK];
-       if (r.you == YOU.PAPER) score += points[ME.SCICCOR];
-     }
-  });
-  return score
-};
+const p2 = () => prepareData().reduce((score,r) => 
+  score + (r.me === RESULT.TIE 
+    ? points.T + points.TIE[r.you]
+    : r.me === RESULT.LOSE 
+    ? points.L + points.LOSE[r.you]      
+    : points.W + + points.WIN[r.you]
+    )
+  ,0);
 
 module.exports = { p1, p2 };
