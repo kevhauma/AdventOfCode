13831c2fb814dea40e9f0d6bcf9f5c314aebdce4
kevin hauman <kevin.hauman@zinderlabs.com>
2022-12-02 15:01:08 +0100

    added package.json with scripts, can execute only last day

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 const fs = require("fs");
 
-const folders = fs.readdirSync("./");
-folders.sort().forEach((folder) => {
-  if (!folder.includes(".")) {
-    const { p1, p2 } = require(`./${folder}/puzzle.js`);
-    console.log(`===== Excuting ${folder} =====`);
-    
-    console.time("Epoch Part 1");
-    const r1 = p1();
-    console.log(`Part 1:\t\t ${r1}`);
-    console.timeEnd("Epoch Part 1");
+const FIRST_ARG = process.argv[2]
 
-    console.time("Epoch Part 2");
-    const r2 = p2();
-    console.log(`Part 2:\t\t ${r2}`);
-    console.timeEnd("Epoch Part 2");
+const folders = fs.readdirSync("./").sort().filter(f=> f.includes("day"));
+const folders_to_exec = FIRST_ARG === 'all' ? folders : folders.reverse().slice(0,1)
 
-    console.log();
-  }
+folders_to_exec.forEach((folder) => {
+  const { p1, p2 } = require(`./${folder}/puzzle.js`);
+  console.log(`===== Excuting ${folder} =====`);
+
+  console.time("Epoch Part 1");
+  const r1 = p1();
+  console.log(`Part 1:\t\t ${r1}`);
+  console.timeEnd("Epoch Part 1");
+
+  console.time("Epoch Part 2");
+  const r2 = p2();
+  console.log(`Part 2:\t\t ${r2}`);
+  console.timeEnd("Epoch Part 2");
+
+  console.log();
 });
