ca2b0f161608c34d1306ba248049300bfcc78205
kevin hauman <kevin.hauman@zinderlabs.com>
2022-12-06 10:06:03 +0100

    timing table instead of inline

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
 const fs = require("fs");
 
-const FIRST_ARG = process.argv[2]
+const FIRST_ARG = process.argv[2];
 
-const folders = fs.readdirSync("./").sort().filter(f=> f.includes("day"));
-const folders_to_exec = FIRST_ARG === 'all' ? folders : folders.reverse().slice(0,1)
+const folders = fs
+  .readdirSync("./")
+  .sort()
+  .filter((f) => f.includes("day"));
+const folders_to_exec =
+  FIRST_ARG === "all" ? folders : folders.reverse().slice(0, 1);
 
+const times = [["day  :"], ["part1:"], ["part2:"]];
 folders_to_exec.forEach((folder) => {
   const { p1, p2 } = require(`./${folder}/puzzle.js`);
   console.log(`===== Excuting ${folder} =====`);
 
-  console.time("Epoch Part 1");
+  const t1p1 = performance.now();
   const r1 = p1();
-  console.log(`Part 1:\t\t ${r1}`);
-  console.timeEnd("Epoch Part 1");
+  console.log(`Part 1:\t ${r1}`);
+  const t2p1 = performance.now();
 
-  console.time("Epoch Part 2");
+  const t1p2 = performance.now();
   const r2 = p2();
-  console.log(`Part 2:\t\t ${r2}`);
-  console.timeEnd("Epoch Part 2");
+  console.log(`Part 2:\t ${r2}`);
+  const t2p2 = performance.now();
 
   console.log();
+  times[0].push(folder + " ");
+  times[1].push((t2p1 - t1p1).toFixed(3) + "ms");
+  times[2].push((t2p2 - t1p2).toFixed(3) + "ms");
+});
+
+times.forEach((time) => {
+  console.log(time.join("\t | "));
 });
