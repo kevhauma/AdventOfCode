dc7f40e677d5c2c07c3cb46ab9aff111c4b213ee
kevin hauman <kevin.hauman@zinderlabs.com>
2022-12-09 21:58:08 +0100

    testing with visuals

--- day9/puzzle.js
+++ day9/puzzle.js
@@ -2,115 +2,148 @@ const fs = require("fs");
 
 const prepareData = () => {
   return fs
-    .readFileSync("./day9/test.txt", { encoding: "utf8" })
+    .readFileSync("./day9/input.txt", { encoding: "utf8" })
     .trim()
     .split(/\r?\n/g)
-    .map(l=>(l.split(" ")));
+    .map((l) => l.split(" "));
 };
 
+const moveHead = (start, dir) => {
+  let newHead = null;
+  switch (dir) {
+    case "R":
+      newHead = [start[0] + 1, start[1]];
+      break;
+    case "L":
+      newHead = [start[0] - 1, start[1]];
+      break;
+    case "U":
+      newHead = [start[0], start[1] + 1];
+      break;
+    case "D":
+      newHead = [start[0], start[1] - 1];
+      break;
+  }
+  return newHead;
+};
 
-const moveHead = (start,dir) => {
-  let newHead = null
-switch (dir) {
-  case "R":
-    newHead = [start[0] + 1, start[1]];
-    break;
-  case "L":
-    newHead = [start[0] - 1, start[1]];
-    break;
-  case "U":
-    newHead = [start[0], start[1] + 1];
-    break;
-  case "D":
-    newHead = [start[0], start[1] - 1];
-    break;
-}
-return newHead
-}
+const moveTail = (previous, next, isHead) => {
+  let tail = next;
+  const tailDiff = [next[0] - previous[0], next[1] - previous[1]];
+  //right follow
+  if (tailDiff[0] < -1) tail = [previous[0] - 1, previous[1]];
+  //left follow
+  if (tailDiff[0] > 1) tail = [previous[0] + 1, previous[1]];
+  //up follow
+  if (tailDiff[1] < -1) tail = [previous[0], previous[1] - 1];
+  //down follow
+  if (tailDiff[1] > 1) tail = [previous[0], previous[1] + 1];
 
-const moveTail = (previous,next) => {
-  let tail = next
- const tailDiff = [next[0] - previous[0], next[1] - previous[1]];
- //right follow
- if (tailDiff[0] < -1) tail = [previous[0] - 1, previous[1]];
- //left follow
- if (tailDiff[0] > 1) tail = [previous[0] + 1, previous[1]];
- //up follow
- if (tailDiff[1] < -1) tail = [previous[0], previous[1] - 1];
- //down follow
- if (tailDiff[1] > 1) tail = [previous[0], previous[1] + 1];
+  //right corner
+  if (tailDiff[0] < -1 && tailDiff[1] !== 0) {
+    tail = [previous[0] - 1, previous[1]];
+   
+  }
+  //left corner
+  if (tailDiff[0] > 1 && tailDiff[1] !== 0) {
+    tail = [previous[0] + 1, previous[1]];
+    
+  }
+  //up corner
+  if (tailDiff[1] < -1 && tailDiff[0] !== 0) {
+    tail = [previous[0], previous[1] - 1];
+   
+     
+  }
+  //down corner
+  if (tailDiff[1] > 1 && tailDiff[0] !== 0) {
+    tail = [previous[0], previous[1] + 1];
+    
+  }
 
- //right corner
- if (tailDiff[0] < -1 && tailDiff[1] !== 0) tail = [previous[0] - 1, previous[1]];
- //down corner
- if (tailDiff[0] > 1 && tailDiff[1] !== 0) tail = [previous[0] + 1, previous[1]];
- //up corner
- if (tailDiff[1] < -1 && tailDiff[0] !== 0) tail = [previous[0], previous[1] - 1];
- //down corner
- if (tailDiff[1] > 1 && tailDiff[0] !== 0) tail = [previous[0], previous[1] + 1];
+  return tail;
+};
 
- return tail
-}
+const visualize = (visited, max,min) => {
+  const uniques = Array.from(new Set(visited)).map((x) => x.split(","));
+  for (let y = max[1]; y >= min[1]; y--) {
+    let row = "";
+    for (let x = min[0]; x <= max[0]; x++) {
+      const found = uniques.find((uq) => uq[0] == x && uq[1] == y);
+      row += found ? "# " : ". ";
+    }
+    console.log(row);
+  }
+};
 
 /*
 Part one
 */
-const p1 = () =>{
- const data = prepareData();
- const visited = ["0,0"]
-let head = [0,0]
-let tail = [0,0]
-data.forEach(([dir,dis])=>{
-Array.from(Array(parseInt(dis))).forEach((_,index)=>{
-  let newHead =moveHead(head,dir);
-  
-  console.log(newHead)
-  
-  const newTail = moveTail(newHead,tail)
+const p1 = () => {
+  // const data = prepareData();
+  // const visited = ["0,0"];
+  // let head = [0, 0];
+  // let tail = [0, 0];
 
-  visited.push(newTail.join(","));
-  head = newHead;
-  tail = newTail;
-})
-})
-return new Set(visited).size
-}
+  // let max = [0, 0];
+  // let min = [0, 0];
+
+  // data.forEach(([dir, dis]) => {
+  //   Array.from(Array(parseInt(dis))).forEach((_, i) => {
+  //     let newHead = moveHead(head, dir);
+
+  //     const newTail = moveTail(newHead, tail, !i);
+
+  //     visited.push(newTail.join(","));
+  //     if (newTail[0] > max[0]) max[0] = newTail[0];
+  //     if (newTail[1] > max[1]) max[1] = newTail[1];
+  //     if (newTail[0] < min[0]) min[0] = newTail[0];
+  //     if (newTail[1] < min[1]) min[1] = newTail[1];
+  //     head = newHead;
+  //     tail = newTail;
+  //   });
+  // });
+  // visualize(visited, max,min);
+  // return new Set(visited).size;
+};
 
 /*
 Part two
 */
 
-const p2 = () =>{
-  const TAIL_SIZE = 9
- 
-const data = prepareData();
-const visited = ["0,0"];
-let head = [0, 0];
- let tails = Array.from(Array(TAIL_SIZE)).map((x) => [0, 0]);
-data.forEach(([dir, dis]) => {
-  Array.from(Array(parseInt(dis))).forEach((_,moveIndex) => {
-    let newHead = moveHead(head,dir)
-    console.log(newHead);
+const p2 = () => {
+  const TAIL_SIZE = 9;
 
-    let fullSnake = [newHead, ...tails];
-    let tailIndex = 0;
-    let prevTail = fullSnake[1];
-    let newTails = [];
-    while (prevTail) {
-      ++tailIndex;
-      let tail = fullSnake[tailIndex];
-      if (!tail) return;
-      const newTail = moveTail(prevTail,tail)
-      // console.log(fullSnake[tailIndex], tail);
-      visited.push(newTail.join(","));
-      newTails.push(newTail);
+  const data = prepareData()//.slice(0,2);
+  const visited = ["0,0"];
+  let head = [0, 0];
+  let tails = Array.from(Array(TAIL_SIZE)).map((x) => [0, 0]);
 
-      prevTail = newTail;
-    }
-    head = newHead;
-    tails = [...newTails];
+  let max = [0, 0];
+  let min = [0, 0];
+  data.forEach(([dir, dis]) => {
+    Array.from(Array(parseInt(dis))).forEach((_) => {
+      let newHead = moveHead(head, dir);
+
+      let previousTail = head;
+      const newTails = tails.map((tail,i) => {
+        const newTail = moveTail(previousTail, tail);
+        if(i+1===TAIL_SIZE)
+          visited.push(newTail.join(","));
+        if (newTail[0] > max[0]) max[0] = newTail[0];
+        if (newTail[1] > max[1]) max[1] = newTail[1];
+        if (newTail[0] < min[0]) min[0] = newTail[0];
+        if (newTail[1] < min[1]) min[1] = newTail[1];
+        previousTail = newTail;
+        
+        return newTail;
+      });
+      head = newHead;
+      tails = [...newTails];
+    });
   });
-});
- return new Set(visited).size;
-}
+
+  visualize(visited, max,min);
+  return new Set(visited).size;
+};
 module.exports = { p1, p2 };
