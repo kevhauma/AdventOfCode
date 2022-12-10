d14ffe3577745fdffe10b301976bc1700d3226c0
Kevin Hauman <kevin.hauman@zinderlabs.com>
2022-12-10 18:06:43 +0100

    day10, comeplete

--- day10/puzzle.js
+++ day10/puzzle.js
@@ -1,14 +1,38 @@
 const fs = require("fs");
 
 const prepareData = () => {
-  return fs
-    .readFileSync("./day10/test.txt", { encoding: "utf8" })
+  const strengths = [];
+  let cycleNumber = 0;
+  fs.readFileSync("./day10/input.txt", { encoding: "utf8" })
     .trim()
     .split(/\r?\n/g)
     .map((l) => ({
       instr: l.split(" ")[0],
       strength: parseInt(l.split(" ")[1]),
-    }));
+    }))
+    .forEach((entry) => {
+      let currentStrengthCycle = strengths[strengths.length - 1];
+      let currentStrength = currentStrengthCycle ? currentStrengthCycle.end : 1;
+
+      if (entry.instr === "addx") {
+        strengths.push({
+          start: currentStrength,
+          cycleNumber: ++cycleNumber,
+          end: currentStrength,
+        });
+        strengths.push({
+          start: currentStrength,
+          cycleNumber: ++cycleNumber,
+          end: currentStrength + entry.strength,
+        });
+      } else
+        strengths.push({
+          start: currentStrength,
+          cycleNumber: ++cycleNumber,
+          end: currentStrength,
+        });
+    });
+  return strengths.sort((a, b) => a.cycleNumber - b.cycleNumber);
 };
 
 /*
@@ -16,36 +40,18 @@ Part one
 */
 
 const p1 = () => {
-  const data = prepareData()
-  const strengths = [];
-
-  data.forEach((entry) => {
-    let currentStrength = strengths[strengths.length - 1]
-    if(!currentStrength)
-        currentStrength = 1
-
-    if (entry.instr === "addx") {
-      const newStrength = currentStrength + entry.strength;
- 
-      strengths.push(currentStrength, newStrength);
-    }
-    else strengths.push(currentStrength)
-  });
+  const strengths = prepareData();
 
   let indexToCheck = 20;
-  let previousvalues = []
-  
+
   return strengths
-    .map((str, i) => {   
-        previousvalues.push(str)    
-     if( i + 1 === indexToCheck) { 
-        const c = str * indexToCheck
-        console.log(indexToCheck, "*", str, "=", c);
-      indexToCheck += 40;
-previousvalues =[]
-      return c
-    }
-    return null;     
+    .map((str) => {
+      if (str.cycleNumber === indexToCheck) {
+        const c = str.start * indexToCheck;
+        indexToCheck += 40;
+        return c;
+      }
+      return null;
     })
     .filter(Boolean)
     .reduce((sum, curr) => sum + curr);
@@ -55,7 +61,26 @@ previousvalues =[]
 Part two
 */
 const p2 = () => {
-  const data = prepareData();
+  const SCREEN_WIDTH = 40;
+  const strengths = prepareData();
+  let outputLines = ["\n"];
+  let line = [];
+  let spritePos = [0, 1, 2];
+  strengths.forEach((str) => {
+    const pixel = str.cycleNumber % SCREEN_WIDTH;
+
+    //pixel drawn is one position behind on cyclenumber
+    if (spritePos.includes(pixel - 1)) line.push("#");
+    else line.push(".");
+    if (!(str.cycleNumber % SCREEN_WIDTH)) {
+      outputLines.push(line.join(""));
+      line = [];
+      spritePos = [0, 1, 2];
+    }
+    
+    if (str.start !== str.end) spritePos = [str.end - 1, str.end, str.end + 1];
+  });
+  return outputLines.join("\n");
 };
 
 module.exports = { p1, p2 };
