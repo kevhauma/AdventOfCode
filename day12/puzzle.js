c25c24b2963ce832618d02870fdb838b5d70ab19
Kevin Hauman <kevin.hauman@zinderlabs.com>
2022-12-12 14:11:34 +0100

    day 12, p1

--- day12/puzzle.js
+++ day12/puzzle.js
@@ -1,24 +1,15 @@
 const fs = require("fs");
 
-const vizualise = (path) => {
-  const width = Math.max(...path.map((p) => p.x)) + 1;
-  const height = Math.max(...path.map((p) => p.y)) + 1;
-  const arrows = path.map((p, i) => {
-    let arrow = ".";
-    const nx = path[i + 1];
-    if (!nx) arrow = "0";
-    else if (p.x > nx.x) arrow = "<";
-    else if (p.x < nx.x) arrow = ">";
-    else if (p.y > nx.y) arrow = "^";
-    else if (p.y < nx.y) arrow = "V";
-    return { arrow, x: p.x, y: p.y };
-  });
 
+const findNode = (nodes, x, y) => {
+  return nodes.find((n) => x === n.x && y === n.y);
+};
+const vizualise = (nodes,height,width) => {
   for (let y = 0; y < height; y++) {
     let line = "";
     for (let x = 0; x < width; x++) {
-      const found = arrows.find((a) => a.x === x && a.y === y);
-      line += found ? found.arrow : ".";
+      const found =findNode(nodes,x,y);
+      line += found ? found.letter : ".";
     }
     console.log(line);
   }
@@ -31,72 +22,110 @@ const prepareData = () => {
     .split(/\r?\n/g)
     .map((x) => x.split(""));
 };
-const cell = (letter, x, y) => ({ letter, x, y });
-const findNeighboursToGoTo = (data, currCell, prevCells, width, heigth) => {
+const makeNode = (letter, x, y, distance = Infinity) => ({
+  letter,
+  x,
+  y,
+  visited: false,
+  distance,
+  tentDistance: 1,
+});
+
+const findNeighboursToGoTo = (nodes, currCell, width, heigth) => {
   const { x, y, letter } = currCell;
   const neighbours = [];
-  if (x !== width - 1) neighbours.push(cell(data[y][x + 1], x + 1, y));
-  if (x !== 0) neighbours.push(cell(data[y][x - 1], x - 1, y));
-  if (y !== heigth - 1) neighbours.push(cell(data[y + 1][x], x, y + 1));
-  if (y !== 0) neighbours.push(cell(data[y - 1][x], x, y - 1));
+  if (x !== width) {
+    const rightNeighbour = findNode(nodes, x + 1, y);
+    if (rightNeighbour) {
+      neighbours.push(rightNeighbour);
+    }
+  }
+  if (x !== 0) {
+    const leftNeighbour = findNode(nodes, x - 1, y);
+    if (leftNeighbour ) {
+      neighbours.push(leftNeighbour);
+    }
+  }
+  if (y !== heigth) {
+    const bottomNeighbour = findNode(nodes, x, y + 1);
+    if (bottomNeighbour) {
+      neighbours.push(bottomNeighbour);
+    }
+  }
+  if (y !== 0) {
+    const topNeighbour = findNode(nodes, x, y - 1);
+    if (topNeighbour) {
+      neighbours.push(topNeighbour);
+    }
+  }
 
   return neighbours.filter((n) => {
+    //remove neighbours who's character is more than one up
     //if letter is E, check on letter z
     if (n.letter === "E") return "z".charCodeAt(0) - letter.charCodeAt(0) <= 1;
-    else
-      return (
-        n.letter.charCodeAt(0) - letter.charCodeAt(0) <= 1 &&
-        !prevCells.find((c) => c.x === n.x && c.y === n.y)
-      );
+    else return n.letter.charCodeAt(0) - letter.charCodeAt(0) <= 1;
   });
 };
 
-const findPath = (data, currCell, prevCells, width, heigth, end) => {
-  const previousCells = [...prevCells, currCell];
-  const neighbours = findNeighboursToGoTo(
-    data,
-    currCell,
-    prevCells,
-    width,
-    heigth
-  );
 
-  if (neighbours.find((n) => n.letter === "E")) {
-    return [...previousCells, end];
-  }
-    console.log("pathLength: ", previousCells.length);
-//vizualise(previousCells)
-const paths = []
-for (const n of neighbours) {
-    const p = findPath(data, n, previousCells, width, heigth, end);
-    if(p?.length)
-    paths.push(p)
-}
-  const nPath = paths
-    .sort((a, b) => a.length - b.length)[0];
-    
-  return nPath;
-};
+// findDistance = (nodes, startNode, endNodes) {
+
+// }
 
 /*
 Part one
 */
 const p1 = () => {
   const data = prepareData();
-  const width = data[0].length;
-  const heigth = data.length;
-  let startCell = { letter: "a", x: -1, y: -1 };
-  let endCell = { letter: "E", x: -1, y: -1 };
+
+  let startNode = { letter: "a", x: -1, y: -1 };
+  let endNode = { letter: "E", x: -1, y: -1 };
+  const allNodes = [];
+
   data.forEach((row, y) => {
-    let x = row.findIndex((letter) => letter === "S");
-    if (x !== -1) startCell = { ...startCell, x, y };
-    x = row.findIndex((letter) => letter === "E");
-    if (x !== -1) endCell = { ...endCell, x, y };
+    row.forEach((letter, x) => {
+      if (letter === "S" || ) {
+        startNode.push = makeNode("a", x, y, 0);
+        allNodes.push(startNode)
+      } else if (letter === "E") {
+        endNode = makeNode("E", x, y);
+        allNodes.push(endNode)
+      } else allNodes.push(makeNode(letter, x, y));
+    });
   });
-  const foundPath = findPath(data, startCell, [], width, heigth, endCell);
-  
-  vizualise(foundPath);
-  return foundPath.length -1
+
+  const width = Math.max(...allNodes.map((p) => p.x)) + 1
+  const height = Math.max(...allNodes.map((p) => p.y)) + 1
+
+  let nodesToVisit = [startNode];
+let unvisitedNodes = [...allNodes]
+  while (!findNode(nodesToVisit, endNode.x, endNode.y)) {
+    const startPerf = performance.now()
+    const nodesToVisitThisRound = [...nodesToVisit];
+    nodesToVisit = [];
+    nodesToVisitThisRound.forEach((currNode) => {
+      const neighbours = findNeighboursToGoTo(
+        unvisitedNodes,
+        currNode,
+        width,
+        height
+      );
+
+      neighbours.forEach((neigh) => {
+        neigh.visited = true;
+        const newDistance = neigh.tentDistance + currNode.distance;
+        if (newDistance < neigh.distance) {
+          neigh.distance = newDistance;
+        }
+      });
+      nodesToVisit.push(...neighbours);
+      currNode.visited = true;
+      currNode.distance += 1;
+      unvisitedNodes = unvisitedNodes.filter((n) => !n.visited);
+    });   
+   
+  }
+  return endNode.distance
 };
 
 /*
@@ -107,4 +136,4 @@ const p2 = () => {
 };
 
 module.exports = { p1, p2 };
-p1()
+p1();
