a204c4341a48607ba4cceb8b8873444cb2f504ec
kevin hauman <kevin.hauman@zinderlabs.com>
2022-12-13 12:12:08 +0100

    DAY 10, WHY YOU NO WORKY"

--- day13/puzzle.js
+++ day13/puzzle.js
@@ -2,42 +2,45 @@ const fs = require("fs");
 
 const prepareData = () => {
   return fs
-    .readFileSync("./day13/test.txt", { encoding: "utf8" })
+    .readFileSync("./day13/input.txt", { encoding: "utf8" })
     .trim()
     .split(/\r?\n\r?\n/g)
     .map((pairs) => pairs.split(/\r?\n/g).map((signal) => JSON.parse(signal)));
 };
 
 const compareArray = (firstArr, secondArr, index) => {
-  //console.log("=========");
-  //console.log(firstArr,"[vs]" ,secondArr);
-  ++index
-  if (index > 8) return null;
+  console.log("=========");
+  console.log("index:",index)  
+  console.log(firstArr, "[vs]", secondArr);
+  if (index === -1) return -1;
+  if(firstArr.length === 0) return ++index
+  ++index;
+  //if (index > 8) return null;
   for (const fI in firstArr) {
     let first = firstArr[fI];
     let second = secondArr[fI];
+    console.log("fs:", first,"-", second);
     if (!first) {
-      break;
+     return ++index
     };
     if (!second){
-       index = -1
-       break
+       return -1
       };
-    //console.log("fs:",first, second);
     //check numbers if not arrays
     if (!Array.isArray(first) && !Array.isArray(second)) {
       if (first < second) {
-         break
+         return ++index;
       } else if (first > second) {
-        index = -1;
-        break
+       return -1
       }
+      //if numbers are equal, dont make em into arrays
       else continue;
     }
     if (!Array.isArray(first)) first = [first];
     if (!Array.isArray(second)) second = [second];
-    //console.log("newArrays:",first,second)
+   console.log("newArrays:",first,second)
     index = compareArray(first, second, index);
+    if(index === -1) return -1
   }
   return index
 };
@@ -48,9 +51,9 @@ Part one
 const p1 = () => {
   const data = prepareData();
   return data.reduce((sum, pair,index) => {
-    //console.log("===***===***===")
-    const rightOrder = compareArray(pair[0], pair[1], 0);
-    //console.log(index + 1,sum, rightOrder, pair);
+    console.log("===***===***===")
+    const rightOrder = compareArray(pair[0], pair[1], 1);
+    console.log(index + 1,rightOrder > 0?"y":"n",sum);
     if (rightOrder > 0) return sum + index+1;
     else return sum
   },0);
