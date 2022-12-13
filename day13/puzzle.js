ac493899ed6f59a724cff42a0160201b604ad6a6
kevin hauman <kevin.hauman@zinderlabs.com>
2022-12-13 10:59:23 +0100

    day13 p1 works on test, not on real input

--- day13/puzzle.js
+++ day13/puzzle.js
@@ -9,27 +9,37 @@ const prepareData = () => {
 };
 
 const compareArray = (firstArr, secondArr, index) => {
-  let bothNot = false;
+  //console.log("=========");
+  //console.log(firstArr,"[vs]" ,secondArr);
+  ++index
+  if (index > 8) return null;
   for (const fI in firstArr) {
     let first = firstArr[fI];
     let second = secondArr[fI];
-
+    if (!first) {
+      break;
+    };
+    if (!second){
+       index = -1
+       break
+      };
+    //console.log("fs:",first, second);
     //check numbers if not arrays
     if (!Array.isArray(first) && !Array.isArray(second)) {
       if (first < second) {
-        return ++index
+         break
+      } else if (first > second) {
+        index = -1;
+        break
       }
-      if (first > second) {
-        return -1
-      }
+      else continue;
     }
     if (!Array.isArray(first)) first = [first];
     if (!Array.isArray(second)) second = [second];
-
-     return compareArray(first, second, ++index);
+    //console.log("newArrays:",first,second)
+    index = compareArray(first, second, index);
   }
-
- 
+  return index
 };
 
 /*
@@ -37,6 +47,13 @@ Part one
 */
 const p1 = () => {
   const data = prepareData();
+  return data.reduce((sum, pair,index) => {
+    //console.log("===***===***===")
+    const rightOrder = compareArray(pair[0], pair[1], 0);
+    //console.log(index + 1,sum, rightOrder, pair);
+    if (rightOrder > 0) return sum + index+1;
+    else return sum
+  },0);
 };
 
 /*
