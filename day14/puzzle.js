f2e5c24e8dcb7d65ea50d80c59f1b3bd5843b543
kevin hauman <kevin.hauman@zinderlabs.com>
2022-12-14 13:44:25 +0100

    day14, major optimisation -> 4minutes to 1.3 seconds

--- day14/puzzle.js
+++ day14/puzzle.js
@@ -5,19 +5,17 @@ const visualize = (walls, sand, min, max) => {
     let line = "";
     for (let x = min.x; x <= max.x; x++) {
       if (x === chute.x && y === chute.y) line += "+";
-      else if (walls.find((w) => w.x === x && w.y === y)) line += "#";
-      else if (sand.find((s) => s.x === x && s.y === y)) line += "0";
+      else if (walls[`${x},${y}`] = true) line += "#";
+      else if (sand[`${x},${y}`]) line += "0";
       else line += ".";
     }
     console.log(line);
   }
 };
-const occupiedCheck = (space, maxY,part2) => (s) => part2 && space.y >= maxY + 2 ? true : s.x === space.x && s.y === space.y
+
 const isSpaceOccupied = (space, walls, sand, maxY, part2) =>
-  Boolean(
-    walls.find(occupiedCheck(space, maxY, part2)) ||
-      sand.find(occupiedCheck(space, maxY, part2))
-  );
+  walls[`${space.x},${space.y}`] || sand[`${space.x},${space.y}`] ||
+  (part2 && space.y >= maxY + 2);
 
 const getNextSpace = (start, walls, sand, maxY, part2) => {
   const availableNextSpaces = [
@@ -28,6 +26,7 @@ const getNextSpace = (start, walls, sand, maxY, part2) => {
   for (const availableNextSpace of availableNextSpaces) {
     if (!part2 && availableNextSpace.y > maxY)
       return "AAAAAAH I CANT STOP FALLING";
+      //console.log(walls)
     if (!isSpaceOccupied(availableNextSpace, walls, sand, maxY, part2)) {
       return getNextSpace(availableNextSpace, walls, sand, maxY, part2);
     }
@@ -45,7 +44,12 @@ const prepareData = (inputPath) => {
         y: parseInt(coords.split(",")[1]),
       }))
     );
-  const walls = [];
+  const walls = {};
+  let maxX = 0
+  let minX = Infinity;
+  let maxY = 0;
+  let minY = Infinity;
+
   wallCommands.forEach((cmds) => {
     let prevCmd = cmds.pop();
     while (cmds.length) {
@@ -56,19 +60,19 @@ const prepareData = (inputPath) => {
         prevCmd.y < currCmd.y ? [currCmd.y, prevCmd.y] : [prevCmd.y, currCmd.y];
 
       for (let x = smallestX; x <= largestX; x++) {
-        walls.push({ x, y: currCmd.y });
+        walls[`${x},${currCmd.y}`] = true;
       }
       for (let y = smallestY; y <= largestY; y++) {
-        walls.push({ x: currCmd.x, y });
+        walls[`${currCmd.x},${y}`] = true;
       }
-
+      maxX = largestX > maxX ? largestX : maxX
+      minX = smallestX < minX ? smallestX : minX
+      maxY = largestY > maxY ? largestY : maxY;
+      minY = smallestY < minY ? smallestY : minY;
       prevCmd = currCmd;
     }
   });
-  const maxX = Math.max(...walls.map((w) => w.x));
-  const minX = Math.min(...walls.map((w) => w.x));
-  const maxY = Math.max(...walls.map((w) => w.y));
-  const minY = Math.min(...walls.map((w) => w.y));
+  
   return { walls, min: { x: minX, y: minY }, max: { x: maxX, y: maxY } };
 };
 
@@ -77,15 +81,15 @@ Part one
 */
 const p1 = (inputPath) => {
   const data = prepareData(inputPath);
-  const sand = [];
+  
+  const sand = {};
   while (true) {
     const sandFall = getNextSpace(chute, data.walls, sand, data.max.y);
-   
-    if (sandFall.x) sand.push(sandFall);
+    if (sandFall.x) sand[`${sandFall.x},${sandFall.y}`] = true
     else break;
   }
-  //visualize(data.walls, sand, data.min, data.max);
-  return sand.length;
+  //$visualize(data.walls, sand, data.min, data.max);
+  return Object.keys(sand).length;
 };
 
 /*
@@ -97,11 +101,11 @@ const p2 = (inputPath) => {
   const sand = [];
   while (true) {
     const sandFall = getNextSpace(chute, data.walls, sand, data.max.y, true);
-    sand.push(sandFall);    
+    sand[`${sandFall.x},${sandFall.y}`] = true;
     if (sandFall.x === chute.x && sandFall.y === chute.y) break;
   }
   //visualize(data.walls, sand, data.min, data.max);
-  return sand.length;
+  return Object.keys(sand).length;
 };
 
 module.exports = { p1, p2 };
