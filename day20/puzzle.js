d4ad472e260527fd859208e4d8c756a3e89eeba3
kevin hauman <kevin.hauman@zinderlabs.com>
2022-12-23 18:14:39 +0100

    small changes

--- day20/puzzle.js
+++ day20/puzzle.js
@@ -20,8 +20,8 @@ const p1 = (inputString, inputPath) => {
     if (x >= 0) newIndex = oldIndex + x + 1;
     else newIndex = oldIndex + x - 1;
 
-    if (newIndex < 0) newIndex = firstArray.length + newIndex + 1;
-    if (newIndex > firstArray.length) newIndex = newIndex - firstArray.length;
+    if (newIndex < 0) newIndex = firstArray.length+((newIndex) % firstArray.length);
+    newIndex =( newIndex % firstArray.length) -1;
 
     resultArray[oldIndex] = { x: "_" };
     const firstPart = resultArray.slice(0, newIndex);
@@ -31,13 +31,15 @@ const p1 = (inputString, inputPath) => {
       ({ x }) => x !== "_"
     );
   });
+  console.log(resultArray.map(({x})=>x).join(" "))
 
   const placesToSum = [1000, 2000, 3000];
   const indexOfZero = resultArray.findIndex(({ x }) => x === 0);
-return placesToSum.reduce((sum,place)=> sum + resultArray[(indexOfZero + place) % firstArray.length].x
-,0)
-
-
+  return placesToSum.reduce(
+    (sum, place) =>
+      sum + resultArray[(indexOfZero + place) % firstArray.length].x,
+    0
+  );
 };
 
 /*
