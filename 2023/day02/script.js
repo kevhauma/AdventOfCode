5c869811e7e0d645bfa4148f8a023d25d0a511f9
kevin hauman <kevin.hauman@zinderlabs.com>
2023-12-02 14:00:00 +0100

    2023-02

---
+++ 2023/day02/script.js
@@ -0,0 +1,78 @@
+const fs = require("fs");
+
+const max = { red: 12, green: 13, blue: 14 };
+
+const prepareData = () => {
+  const input = fs.readFileSync("./input.txt", { encoding: "utf8" });
+  const lines = input
+    .split(/\r?\n/g)
+    .filter(Boolean)
+    .reduce((games, line) => {
+      const [game, setLine] = line.split(":");
+      return {
+        ...games,
+        [game.replace(/[^0-9]/g, "")]: setLine.split(";").map((set) =>
+          set
+            .split(",")
+            .map((block) => block.trim())
+            .reduce((blocks, block) => {
+              const [amount, color] = block.split(" ");
+              return { ...blocks, [color]: parseInt(amount) };
+            }, {})
+        ),
+      };
+    }, {});
+
+  return lines;
+};
+
+/*
+Part one
+*/
+
+const p1 = (inputString, inputPath) => {
+  const games = prepareData(inputString, inputPath);
+  return (filteredGames = Object.keys(games)
+    .filter((gameKey) =>
+      games[gameKey].every((blocks) =>
+        Object.entries(blocks).every(([color, amount]) => amount <= max[color])
+      )
+    )
+    .reduce((sum, curr) => sum + parseInt(curr), 0));
+};
+
+/*
+Part two
+*/
+const p2 = (inputString, inputPath) => {
+  const games = prepareData(inputString, inputPath);
+  return Object.keys(games)
+    .map((gameKey) =>
+      games[gameKey].reduce(
+        (biggest, block) => ({
+          red:
+            block.red !== undefined && biggest.red < block.red
+              ? block.red
+              : biggest.red,
+          green:
+            block.green !== undefined && biggest.green < block.green
+              ? block.green
+              : biggest.green,
+          blue:
+            block.blue !== undefined && biggest.blue < block.blue
+              ? block.blue
+              : biggest.blue,
+        }),
+        { blue: -Infinity, red: -Infinity, green: -Infinity }
+      )
+    )
+    .map((block) =>
+      Object.values(block).reduce((multi, value) => multi * value, 1)
+    )
+    .reduce((sum, value) => sum + value);
+};
+
+console.log(p1());
+console.log(p2());
+
+module.exports = { p1, p2 };
