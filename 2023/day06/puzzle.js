c06d3a3703fd225db4afc6d4891e3f94d56a8c49
kevin hauman <kevin.hauman@zinderlabs.com>
2023-12-06 18:47:38 +0100

    2023-06 smol optimization

--- 2023/day06/puzzle.js
+++ 2023/day06/puzzle.js
@@ -5,8 +5,8 @@ const prepareData = (isP2, inputString) => {
     .reduce((race, line) => {
       const raceCopy = { ...race };
       let [subject, values] = line.split(":");
-      if(isP2) {
-        values = values.replace(/\s/g,"")
+      if (isP2) {
+        values = values.replace(/\s/g, "");
       }
       values
         .split(/\s/g)
@@ -20,22 +20,24 @@ const prepareData = (isP2, inputString) => {
   return lines;
 };
 
+const calculate = (data) =>
+  Object.entries(data)
+    .map(([race, { time, distance }]) => {
+      let won = 0;
 
-const calculate = (data) => Object.entries(data)
-.map(([race, { time, distance }]) => {
-  let won = 0;
-  
-  for (let holdForMs = 0; holdForMs <= time; holdForMs++) {
-    const timeLeft = time - holdForMs
-    const attemptDistance = timeLeft * holdForMs
-    
-    if (attemptDistance > distance) won += 1;
-  };
+      for (let holdForMs = 0; holdForMs <= time; holdForMs++) {
+        // (time-holdForMs) = time left to move after holding
+        // holdForMs = speed to which to go at
+        // distances this attempt > race distance
+        // + 0 if false
+        // + 1 if true
+        won += (time - holdForMs) * holdForMs > distance;
+      }
 
-  return won;
-})
-.filter(Boolean)
-.reduce((mult, curr) => mult * curr);
+      return won;
+    })
+    .filter(Boolean)
+    .reduce((mult, curr) => mult * curr);
 
 /*
 Part one
@@ -43,7 +45,7 @@ Part one
 
 const p1 = (inputString, inputPath) => {
   const data = prepareData(false, inputString, inputPath);
-  return calculate(data)
+  return calculate(data);
 };
 
 /*
@@ -51,7 +53,7 @@ Part two
 */
 const p2 = (inputString, inputPath) => {
   const data = prepareData(true, inputString, inputPath);
-  return calculate(data)
+  return calculate(data);
 };
 
 module.exports = { p1, p2 };
