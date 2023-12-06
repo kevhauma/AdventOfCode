19ac94e4e3d4c4e1e6182e7db350e8961afdbc6a
kevin hauman <kevin.hauman@zinderlabs.com>
2023-12-06 10:15:03 +0100

    2023-06

---
+++ 2023/day06/puzzle.js
@@ -0,0 +1,60 @@
+const prepareData = (isP2, inputString) => {
+  const lines = inputString
+    .split(/\r?\n/g)
+    .filter(Boolean)
+    .reduce((race, line) => {
+      const raceCopy = { ...race };
+      let [subject, values] = line.split(":");
+      if(isP2) {
+        values = values.replace(/\s/g,"")
+      }
+      values
+        .split(/\s/g)
+        .filter(Boolean)
+        .forEach((value, index) => {
+          if (!raceCopy[index]) raceCopy[index] = {};
+          raceCopy[index][subject.toLowerCase()] = parseInt(value);
+        });
+      return raceCopy;
+    }, {});
+  return lines;
+};
+
+
+const calculate = (data) => Object.entries(data)
+.map(([race, { time, distance }]) => {
+  let won = 0;
+  Array.from({ length: time }).forEach((_, holdForMs) => {
+    let speed = 0;
+    let attemptDistance = 0;
+    Array.from({ length: time }).forEach((_, currentTimeStep) => {
+      if (currentTimeStep < holdForMs) speed += acceleration;
+      else attemptDistance += speed;
+    });
+
+    if (attemptDistance > distance) won += 1;
+  });
+
+  return won;
+})
+.filter(Boolean)
+.reduce((mult, curr) => mult * curr);
+
+/*
+Part one
+*/
+const acceleration = 1;
+const p1 = (inputString, inputPath) => {
+  const data = prepareData(false, inputString, inputPath);
+  return calculate(data)
+};
+
+/*
+Part two
+*/
+const p2 = (inputString, inputPath) => {
+  const data = prepareData(true, inputString, inputPath);
+  return calculate(data)
+};
+
+module.exports = { p1, p2 };
