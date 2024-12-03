0fe0bf98b0a13d81d24ffde8e4c4c58ab9296a9f
kevin hauman <kevin.hauman@zinderlabs.com>
2024-12-03 22:55:22 +0100

    2024-02 p2

--- 2024/day02/puzzle.ts
+++ 2024/day02/puzzle.ts
@@ -4,46 +4,60 @@ const prepareData = (inputString: string) => {
   return lines.map((line) => line.split(/\s+/g).map((num) => parseInt(num)));
 };
 
+const isReportSafe = (report: Array<number>) => {
+  const { isSafe } = report.reduce(
+    (result, current, index) => {
+      if (!result.isSafe) return { isSafe: false };
+      if (index === 0) return { isSafe: true };
+
+      const difference = report[index - 1] - current;
+
+      const isSafelyDecreasing = difference > 0 && difference <= 3;
+      const isSafelyIncreasing = difference < 0 && difference >= -3;
+
+      if (!isSafelyDecreasing && !isSafelyIncreasing) return { isSafe: false };
+
+      //if no direction set, return safe
+      if (result.isDecreasing === undefined) {
+        return { isSafe: true, isDecreasing: isSafelyDecreasing };
+      }
+      //isSafe and decreasing
+      if (result.isDecreasing && isSafelyDecreasing) {
+        return { isSafe: true, isDecreasing: true };
+      }
+      //isSafe and increasing
+      if (!result.isDecreasing && isSafelyIncreasing) {
+        return { isSafe: true, isDecreasing: false };
+      }
+      //is not safe
+      return { isSafe: false };
+    },
+    { isSafe: true } as { isSafe: boolean; isDecreasing?: boolean }
+  );
+  return isSafe;
+};
+
+const isSafeWithTolerance = (report: number[]): boolean => {
+  //Thanks grYs51 ≽^•⩊•^≼
+  if (isReportSafe(report)) {
+    return true;
+  }
+  for (let i = 0; i < report.length; i++) {
+    const newReport = report.slice(0, i).concat(report.slice(i + 1));
+    if (isReportSafe(newReport)) {
+      return true;
+    }
+  }
+  return false;
+};
+
 /*
 Part one
 */
 
 export const p1 = (inputString: string) => {
   const data = prepareData(inputString);
-  const reportResults = data
-    .map((report) => {
-      return report.reduce(
-        (result, current, index) => {
-          if (!result.isSafe) return { isSafe: false };
-          if (index === 0) return { isSafe: true };
-
-          const difference = report[index - 1] - current;
-
-          const isSafelyDecreasing = difference > 0 && difference <= 3;
-          const isSafelyIncreasing = difference < 0 && difference >= -3;
-
-          if (!isSafelyDecreasing && !isSafelyIncreasing)
-            return { isSafe: false };
-
-          //if no direction set, return safe
-          if (result.isDecreasing === undefined) {
-            return { isSafe: true, isDecreasing: isSafelyDecreasing };
-          }
-          //isSafe and decreasing
-          if (result.isDecreasing && isSafelyDecreasing) {
-            return { isSafe: true, isDecreasing: true };
-          }
-          //isSafe and increasing
-          if (!result.isDecreasing && isSafelyIncreasing) {
-            return { isSafe: true, isDecreasing: false };
-          }
-          //is not safe
-          return { isSafe: false };
-        },
-        { isSafe: true } as { isSafe: boolean; isDecreasing?: boolean }
-      );
-    })
-    .filter((result) => result.isSafe);
+  const reportResults = data.filter(isReportSafe);
   return reportResults.length;
 };
 
@@ -52,4 +66,6 @@ Part two
 */
 export const p2 = (inputString: string) => {
   const data = prepareData(inputString);
+  const reportResults = data.filter(isSafeWithTolerance);
+  return reportResults.length;
 };
