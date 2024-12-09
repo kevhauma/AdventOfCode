04cec769b28b5fbfca122619a9a0d28c92590bba
kevin hauman <kevin.hauman@zinderlabs.com>
2024-12-09 09:40:11 +0100

    2024-09 p1 less looping

--- 2024/day09/puzzle.ts
+++ 2024/day09/puzzle.ts
@@ -1,18 +1,15 @@
 const prepareData = (inputString: string) => {
   const data = inputString.split("").map(Number)
 
-  const diskMap: Array<{ id: number, block: number, free: number }> = []
+  const diskMap: Array<string> = []
   for (let i = 0; i < data.length; i += 2) {
-    diskMap.push({ id: diskMap.length, block: data[i], free: data[i + 1] })
-  }
+    const file = { id: diskMap.length, block: data[i], free: data[i + 1] }
 
-  const diskMapCompressed = diskMap.map(file => {
     const block = Array.from({ length: file.block }).map(() => file.id).join("")
     const free = Array.from({ length: file.free }).map(() => ".").join("")
-    return `${block}${free}`
-  }).join("").split("")
-
-  return diskMapCompressed;
+    diskMap.push(`${block}${free}`)
+  }
+  return diskMap.join("").split("");
 };
 
 /*
@@ -20,19 +17,22 @@ Part one
 */
 
 export const p1 = (inputString: string) => {
-  const diskmapCompressed = prepareData(inputString);
+  const diskMap = prepareData(inputString);
 
-  // console.log(diskmapCompressed.join(''))
-  for (let i = diskmapCompressed.length - 1; i >= (diskmapCompressed.length) / 2; --i) {
-    const indexOfFirstFreeSpace = diskmapCompressed.indexOf(".")
+  //console.log(diskmapCompressed.join(''))
+  for (let i = diskMap.length - 1; i >= (diskMap.length) / 2; --i) {
+    const indexOfFirstFreeSpace = diskMap.indexOf(".")
     if (indexOfFirstFreeSpace > i)
       break;
-    diskmapCompressed[indexOfFirstFreeSpace] = diskmapCompressed[i]
-    diskmapCompressed[i] = "."
+    diskMap[indexOfFirstFreeSpace] = diskMap[i]
+    diskMap[i] = "."
     // console.log(diskmapCompressed.join(''))
   }
-  // console.log(diskmapCompressed.join(''))
-  return diskmapCompressed.filter(fileId=>fileId!==".").reduce((res,curr,index)=>res + (Number(curr) * index),0)
+  console.log(diskMap.join(''))
+
+  return diskMap
+    .filter(fileId => fileId !== ".")
+    .reduce((res, curr, index) => res + (Number(curr) * index), 0)
 };
 
 /*
