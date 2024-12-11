cfea546c61cfb5dc681091c4adfa5b964a336679
kevin hauman <kevin.hauman@zinderlabs.com>
2024-12-11 11:14:22 +0100

    2024-11 doubled the speed

--- 2024/day11/puzzle.ts
+++ 2024/day11/puzzle.ts
@@ -1,18 +1,22 @@
 const prepareData = (inputString: string) => {
-  const input = inputString.split(/\s+/g).map(Number);
-
-  return input;
+  return inputString.split(/\s+/g)
+    .map(Number)
+    .reduce((dict, curr) => {
+      dict[curr] = 1
+      return dict
+    }, {} as Record<number, number>);
 };
 
 const addToRecord = (record: Record<number, number>, key: number, amount: number = 1) => {
   record[key] = (record[key] || 0) + amount
 }
 
-const blinkStone = (initialStone: number, amountOfBLinks: number) => {
-  let stones: Record<number, number> = { [initialStone]: 1 }
+const blinkStones = (initialStones: Record<number, number>, amountOfBLinks: number) => {
+  let stones: Record<number, number> = initialStones;
 
   for (let blink = 0; blink < amountOfBLinks; blink++) {
     const localStones: Record<number, number> = {}
+
     Object.entries(stones).forEach(([stoneStr, amount]) => {
       const stone = Number(stoneStr)
       if (!stone) {
@@ -24,24 +28,19 @@ const blinkStone = (initialStone: number, amountOfBLinks: number) => {
       if (!(stoneString.length % 2)) {
         const stone1Str = stoneString.split("").splice(0, stoneString.length / 2).join("")
         const stone2Str = stoneString.split("").splice(stoneString.length / 2).join("")
-        //console.log(stone1Str,stone2Str)
+
         addToRecord(localStones, Number(stone1Str), amount)
         addToRecord(localStones, Number(stone2Str), amount)
-
         return
       }
+
       addToRecord(localStones, stone * 2024, amount)
     })
-    //console.log(blink,localStones.join(" "))
+    
     stones = localStones;
   }
   return Object.values(stones).reduce((sum, curr) => sum + curr)
 }
-
-const blinkStones = (initialStones: Array<number> = [], amountOfBLinks: number) => {
-  return initialStones.reduce((sum, stone) => sum + blinkStone(stone, amountOfBLinks), 0)
-}
-
 /*
 Part one
 */
